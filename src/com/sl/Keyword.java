8e4f2f0672756e284b6aefcae1bf1f22656699b7
Warren Crasta <warrencrasta@gmail.com>
2017-05-31 18:28:51 -0500

    Follow by Tweet Keyword DONE

--- src/com/sl/Keyword.java
+++ src/com/sl/Keyword.java
@@ -43,42 +43,11 @@ import com.sl.pojo.UserPojo;
 
 import java.io.IOException;
 
-@WebServlet("/callback")
-public class CallbackServlet extends HttpServlet {
+@WebServlet("/keyword")
+public class Keyword extends HttpServlet {
     private static final long serialVersionUID = 1657390011452788111L;
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// Get twitter object from session
-		Twitter twitter = (Twitter) request.getSession().getAttribute("twitter");
-		//Get twitter request token object from session
-		RequestToken requestToken = (RequestToken) request.getSession().getAttribute("requestToken");
-		String verifier = request.getParameter("oauth_verifier");
-		try {
-			// Get twitter access token object by verifying request token 
-		    AccessToken accessToken = twitter.getOAuthAccessToken(requestToken, verifier);
-		    request.getSession().removeAttribute("requestToken");
-		    
-		    // Get user object from database with twitter user id
-		    UserPojo user = TwitterDAO.selectTwitterUser(accessToken.getUserId());
-		    if(user == null) {
-		       // if user is null, create new user with given twitter details 
-		       user = new UserPojo();
-		       user.setTwitter_user_id(accessToken.getUserId());
-		       user.setTwitter_screen_name(accessToken.getScreenName());
-		       user.setAccess_token(accessToken.getToken());
-		       user.setAccess_token_secret(accessToken.getTokenSecret());
-		       TwitterDAO.insertRow(user);
-		       user = TwitterDAO.selectTwitterUser(accessToken.getUserId());
-		    } else {
-		       // if user already there in database, update access token
-		       user.setAccess_token(accessToken.getToken());
-		       user.setAccess_token_secret(accessToken.getTokenSecret());
-		       TwitterDAO.updateAccessToken(user);
-		    }
-		    request.setAttribute("user", user);
-		} catch (TwitterException | DBException e) {
-		    throw new ServletException(e);
-		} 
-		request.getRequestDispatcher("/index2.html").forward(request, response);
+    	request.getRequestDispatcher("/keyword.jsp").forward(request, response);
     }
 }
---
+++ src/com/sl/PostKeyword.java
@@ -0,0 +1,116 @@
+/*
+ * IDEAS:
+ * Make Twitter username clickable.
+ * Include # of users followed today in Rate Limiting Info
+ */
+
+package com.sl;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import twitter4j.*;
+import java.util.*;
+
+@WebServlet("/postKeyword")
+public class PostKeyword extends HttpServlet {
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		response.setHeader("Connection", "keep-alive");
+		response.setHeader("Transfer-Encoding", "chunked");
+
+		PrintWriter out = response.getWriter();
+		out.println("<!DOCTYPE html>");
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>Results</title>");
+		out.println("<a href='index2.html'>Tools Page</a>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<h2>Results</h2>");
+
+		String keyword = request.getParameter("keyword");
+		String ans = request.getParameter("RTs");
+		int follow = Integer.parseInt(request.getParameter("follow"));
+		int follow2 = Integer.parseInt(request.getParameter("follow2"));
+		Twitter twitter = (Twitter)request.getSession().getAttribute("twitter");
+		if (follow == follow2) {
+			Query query;
+			if(ans.equalsIgnoreCase("yes")) {
+				query = new Query(keyword);
+			} else{
+				query = new Query(keyword + " -filter:retweets");
+			}
+			query.setResultType(Query.RECENT);
+
+			/* Add tweets to a list. */
+			long lastID = Long.MAX_VALUE;
+			ArrayList<Status> tweets = new ArrayList<Status>();
+			while (tweets.size() < follow) {
+				if (follow - tweets.size() > 100)
+					query.setCount(100);
+				else
+					query.setCount(follow - tweets.size());
+				try {
+					QueryResult result = twitter.search(query);
+					tweets.addAll(result.getTweets());
+					for (Status t: tweets)
+						if(t.getId() < lastID) lastID = t.getId();
+				}
+				catch (TwitterException te) {
+					out.println("Couldn't retrieve tweets: " + te);
+					response.flushBuffer();
+					break;
+				};
+				query.setMaxId(lastID-1);
+			}
+			/* Follow all users in the tweets list. */
+			for (int i = 0; i < tweets.size(); i++){
+				Status s = tweets.get(i);
+				try {
+					twitter.createFriendship(s.getUser().getId());
+					out.println(i+1 + ". Following @" + s.getUser().getScreenName() + ".<br />");
+					response.flushBuffer();
+				} catch (TwitterException te) {
+					/* Ignore any errors and keep running. */
+					if (te.toString().contains("You are unable to follow more people at this time.")) {
+						out.println("Couldn't follow users: " + te);
+						response.flushBuffer();
+						break;
+					}
+				}
+			}
+			out.println("<h2>Twitter Rate-Limiting Info</h2>");
+			response.flushBuffer();
+
+			Map<String, RateLimitStatus> rateLimitStatus;
+			try {
+				rateLimitStatus = twitter.getRateLimitStatus();
+				for (String endpoint : rateLimitStatus.keySet()) {
+					RateLimitStatus status = rateLimitStatus.get(endpoint);
+					if (status.getRemaining() < status.getLimit()) {
+						out.println("<strong>Endpoint:</strong> " + endpoint + "<br />");
+						out.println("<strong>Limit:</strong>" + status.getLimit() + "<br />");
+						out.println("<strong>Remaining:</strong>" + status.getRemaining() + "<br />");
+						out.println("<strong>SecondsUntilReset:</strong>" + status.getSecondsUntilReset() + "<br /><br />");
+					}
+				}
+			} catch (TwitterException te) {
+				out.println("Couldn't retrieve rate-limits: " + te);
+				response.flushBuffer();
+			}
+		} else {
+			out.println("The two follow numbers you entered did not match. Click the Back Button on your browser to try again.");
+			response.flushBuffer();
+		}
+		out.println("</body>");
+		out.println("</html>");
+	}
+}
